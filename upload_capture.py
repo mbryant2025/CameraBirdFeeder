29c8992a7cb091648f8fde392582ed4eafaaf7b7
Michael Bryant <mbryant2025@gmail.com>
2022-06-01 20:41:45 -0400

    picamera tied with firebase

---
+++ upload_capture.py
@@ -0,0 +1,24 @@
+import requests
+import time
+import picamera
+from firebase_admin import credentials, initialize_app, storage
+
+
+with picamera.PiCamera() as camera:
+    camera.resolution = (1024, 768)
+
+    camera.capture('image.jpg')
+
+cred = credentials.Certificate('/home/pi/Documents/key.json')
+initialize_app(cred, {'storageBucket': 'camera-bird-feeder-f06ef.appspot.com'})
+
+# Put your local file path 
+fileName = "image.jpg"
+bucket = storage.bucket()
+blob = bucket.blob(fileName)
+blob.upload_from_filename(fileName)
+
+# Opt : if you want to make public access from the URL
+blob.make_public()
+
+
